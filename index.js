e4d06ab1ae36bd790bd8eae42fff2fb97230206a
steve-the-hermit <stevemacharia309@gmail.com>
2023-06-28 18:39:29 +0300

    assignment complete

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Add your code here
+
+function submitData(name, email) {
+    const formData = {
+      name: name,
+      email: email
+    };
+  
+    const configObj = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      },
+      body: JSON.stringify(formData)
+    };
+  
+    return fetch('http://localhost:3000/users', configObj)
+      .then(response => response.json())
+      .then(data => {
+        document.body.innerHTML = data.id;
+      })
+      .catch(error => {
+        document.body.innerHTML = error.message;
+      });
+  }
+  
